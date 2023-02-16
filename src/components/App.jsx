cc23fe72a5f9832d2a212b11ff065510f283d3c6
VozzRoman <looper0500@ukr.net>
2023-02-16 12:47:01 +0200

    adding

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,10 @@ import { Route, Routes } from 'react-router-dom';
 import { Header } from 'components/Header/Header';
 
 const Home = lazy(() => import('../Pages/Home/Home'));
-const Movies = lazy(() => import('../Pages/Movies/Movies'));
+const Movies = lazy(() => import('../Pages/Home/Home'));
 const FilmDeteils = lazy(() => import('../Pages/FilmDeteils/FilmDeteils'));
-const Cast = lazy(() => import('../components/Cast/Cast'));
-const Reviews = lazy(() => import('../components/Reviews/Reviews'));
+const Cast = lazy(() => import('./Cast/Cast'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
 
 // import { Home } from "Pages/Home/Home";
 // import { Movies } from "Pages/Movies/Movies";
